548db4afcff17174735c21312a58b236d290638e
siddiquenaim <naim.niter.tex06@gmail.com>
2023-06-08 18:55:42 +0600

    added all instructors and classes

--- index.js
+++ index.js
@@ -11,22 +11,25 @@ app.use(express.json());
 const instructors = require("./data/instructors.json");
 const classes = require("./data/classes.json");
 
-app.get("/top-classes", (req, res) => {
-  // sorting
+app.get("/all-instructors", (req, res) => {
+  res.send(instructors);
+});
+
+app.get("/popular-classes", (req, res) => {
   const sortedClasses = classes.sort(
     (a, b) => b.numberOfStudents - a.numberOfStudents
   );
 
-  // top-6
-  const topClasses = sortedClasses.slice(0, 6);
+  const popularClasses = sortedClasses.slice(0, 6);
 
-  res.send(topClasses);
+  res.send(popularClasses);
 });
 
 app.get("/popular-instructors", (req, res) => {
   const sortedInstructors = instructors.sort(
     (a, b) => b.numberOfStudents - a.numberOfStudents
   );
+
   const topInstructors = sortedInstructors.slice(0, 6);
   res.send(topInstructors);
 });
